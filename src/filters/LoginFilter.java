2a02fae5c655f2bcf21ee7a8c765fb5eb152216d
Yuta.Kojima <nakatu.oyama@gmail.com>
2020-11-01 18:47:54 +0900

    Add Report new

--- src/filters/LoginFilter.java
+++ src/filters/LoginFilter.java
@@ -44,28 +44,26 @@ public class LoginFilter implements Filter {
         String context_path = ((HttpServletRequest)request).getContextPath();
         String servlet_path = ((HttpServletRequest)request).getServletPath();
 
-        if(!servlet_path.matches("/css.*")) {       // CSSフォルダ内は認証処理から除外する
+        if(!servlet_path.matches("/css.*")) {
             HttpSession session = ((HttpServletRequest)request).getSession();
 
-            // セッションスコープに保存された従業員（ログインユーザ）情報を取得
+
             Employee e = (Employee)session.getAttribute("login_employee");
 
-            if(!servlet_path.equals("/login")) {        // ログイン画面以外について
-                // ログアウトしている状態であれば
-                // ログイン画面にリダイレクト
+            if(!servlet_path.equals("/login")) {
+
+
                 if(e == null) {
                     ((HttpServletResponse)response).sendRedirect(context_path + "/login");
                     return;
                 }
 
-                // 従業員管理の機能は管理者のみが閲覧できるようにする
+
                 if(servlet_path.matches("/employees.*") && e.getAdmin_flag() == 0) {
                     ((HttpServletResponse)response).sendRedirect(context_path + "/");
                     return;
                 }
-            } else {                                    // ログイン画面について
-                // ログインしているのにログイン画面を表示させようとした場合は
-                // システムのトップページにリダイレクト
+            } else {
                 if(e != null) {
                     ((HttpServletResponse)response).sendRedirect(context_path + "/");
                     return;
